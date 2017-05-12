20a9712edf29c7cb714cda3bc4ffbec48c63b514
flame4 <646361781@qq.com>
2017-05-12 23:03:53 +0800

    增加.gitignore配置， 跑了一下cnn的mnist例子.

--- mnist/mnist_cnn.py
+++ mnist/mnist_cnn.py
@@ -4,8 +4,10 @@ from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
 
 
 # 根据定义的形状随机初始化一个变量, 用于当作CNN的权值矩阵
-def weight_variable(shape):
+def weight_variable(shape, name=None):
   initial = tf.truncated_normal(shape, stddev=0.1)
+  if name:
+      return tf.Variable(initial, name=name)
   return tf.Variable(initial)
 
 # 这个用于当作偏移量, 就是公式中的X0.
@@ -32,7 +34,7 @@ def main():
     y = tf.placeholder("float", shape=[None, 10])
 
     # 第一层卷积，卷积在每个5x5的patch中算出32个特征
-    W_conv1 = weight_variable([5, 5, 1, 32])
+    W_conv1 = weight_variable([5, 5, 1, 32], name="W1")
     b_conv1 = bias_variable([32])
     # 为了使用这一层，将x转变维度为4维，第一维度不定，表示batchsize, 第二三维度是高和宽，
     # 第四维度是通道数
@@ -66,6 +68,10 @@ def main():
 
     y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
+    # 保存模型
+    if not tf.gfile.Exists("../asset/mnist_cnn/"):
+        tf.gfile.MakeDirs("../asset/mnist_cnn/")
+
     # 定义后开始训练和评估模型
     print("define over, training start ...")
     cross_entropy = -tf.reduce_sum(y_ * tf.log(y_conv))
@@ -73,16 +79,33 @@ def main():
     correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
     sess.run(tf.initialize_all_variables())
-    for i in range(20000):
+    for i in range(201):
         batch = mnist.train.next_batch(50)
         if i % 100 == 0:
             train_accuracy = accuracy.eval(feed_dict={
                 x: batch[0], y_: batch[1], keep_prob: 1.0})
             print("step %d, training accuracy %g" % (i, train_accuracy))
+
+
+
         train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
     print("test accuracy %g" % accuracy.eval(feed_dict={
         x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+    print(sess.run(tf.get_default_graph().get_tensor_by_name("W1:0")))
+
+    saver = tf.train.Saver({"W1": W_conv1, "B1": b_conv1})
+    saver.save(sess, "../asset/mnist_cnn/mnist",  meta_graph_suffix="model")
+
+
+
+
+
+# 模型加载直接使用
+def test():
+    return
+
 
 print(__name__)
 main()
+#test()
