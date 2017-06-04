02f673959171dd881966c5e73dfa577e359f19b9
flame4 <646361781@qq.com>
2017-06-04 15:05:22 +0800

    添加新文件

---
+++ prisma_style_transfer/fast_transfer_train/arbitrary_transfer_net.py
@@ -0,0 +1,15 @@
+'''
+Arbitrary style transfer
+
+Based on Arbitrary Style Transfer in Real-Time with Adaptive Instance Normalization, Xun Huang.
+
+Author: Liu Yi
+
+'''
+
+import tensorflow as tf
+
+from . import reader
+from . import model
+from . import vgg
+
